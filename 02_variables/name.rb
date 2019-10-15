547e7cf60d64431171e0de9a16520176ef97003d
David Ecklund <davidecklund@protonmail.com>
2019-10-14 21:31:41 -0500

    Second chapter complete

---
+++ 02_variables/name.rb
@@ -0,0 +1,7 @@
+puts "What's your first name, my dear?"
+first_name = gets.chomp
+puts "What's your last name, oh Light-of-my-Soul?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name}, you are a wonderful sight to behold!"
+name = first_name + " " + last_name
+10.times {puts name}
